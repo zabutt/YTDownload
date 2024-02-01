2ccd4ecdcf3001c05a587451bc21a0a42dc554ec
Zulfiqar Ahmed <54955118+zabutt@users.noreply.github.com>
2024-02-01 22:13:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pytube import YouTube
-import tempfile
-import shutil
+import io
+import base64
 
 # Set page title and favicon
 st.set_page_config(page_title="YouTube Downloader", page_icon=":arrow_down:")
@@ -15,11 +15,8 @@ def download_video(youtube_url, download_type):
         else:
             stream = yt.streams.filter(only_audio=True).first()
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            output_path = f"{tmpdir}/{yt.title}.{stream.subtype}"
-            stream.download(output_path)
-
-            return output_path
+        video_bytes = stream.stream_to_buffer()
+        return video_bytes
     except Exception as e:
         st.error(f"An error occurred: {e}")
         return None
@@ -38,12 +35,12 @@ def main():
 
         if st.button("Download"):
             # Download video or audio
-            file_path = download_video(youtube_url, download_type)
+            video_bytes = download_video(youtube_url, download_type)
 
-            if file_path:
+            if video_bytes:
                 # Provide download link
-                st.success(f"Download successful! [Download Link]({file_path})")
+                download_link = f"data:video/{download_type.lower()};base64,{base64.b64encode(video_bytes.getvalue()).decode()}"
+                st.success(f"Download successful! [Download Link]({download_link})")
 
 if __name__ == "__main__":
     main()
-
